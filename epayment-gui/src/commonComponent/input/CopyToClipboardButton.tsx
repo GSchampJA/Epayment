4074dd2a8bd2fca83f95176080aec4c14c86267e
lucascheng24 <lucascheng240@gmail.com>
2023-11-28 17:58:00 +0800

    add some UI to myAddress

---
+++ epayment-gui/src/commonComponent/input/CopyToClipboardButton.tsx
@@ -0,0 +1,31 @@
+import { Button, Snackbar } from '@mui/material'
+import { useState } from 'react'
+
+interface CTCBProps {
+    // isOpen: boolean,
+    copyText: string
+}
+
+const CopyToClipboardButton = (props: CTCBProps) => {
+    const { copyText } = props
+    const [open, setOpen] = useState(false)
+
+    const handleClick = () => {
+      setOpen(true)
+      navigator.clipboard.writeText(copyText)
+    }
+    
+    return (
+        <>
+          <Button className='bgColor-grey' onClick={handleClick}>{copyText}</Button>
+          <Snackbar
+            open={open}
+            onClose={() => setOpen(false)}
+            autoHideDuration={2000}
+            message="Copied to clipboard"
+          />
+        </>
+    )
+}
+
+export default CopyToClipboardButton
