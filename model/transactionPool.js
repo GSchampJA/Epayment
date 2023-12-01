bb48bcd93acd467ed71c2f43f3f77706a6feb1f3
Daniel Yu <93873727+fishmen26@users.noreply.github.com>
2023-12-01 11:22:06 +0800

    Update transactionPool.js

--- model/transactionPool.js
+++ model/transactionPool.js
@@ -2,28 +2,42 @@
 // will be using redis database schema
 import {Schema, Entity} from 'redis-om';
 
-const transactionSchema = new Schema({
+class User extends Entity{
+
+}
+
+const transactionSchema = new Schema(User, {
     chainHeight:{
         type: Int16Array,
-        required: true
     },
     fullNodeList:{
         // full blockchain
-        list: new Schema({
-            address:{
-                type: String,
-                required: true
-            },
-        })
+        blockIndex: Number,
+        blockHeader:{
+            version: Number ,
+            previousBlockHeader: String  ,
+            merkleRoot: String,
+            timeStamp: Number ,
+            difficulty: Number,
+            nonce:  Number
+        },
+        txns: Array,
+        currentBlockHash: String
     },
     neighborList:{
         // the forking blockchain
-        list: new Schema({
-            address:{
-                type: String,
-                required: true
-            },
-        })
+        blockIndex: Number,
+        blockHeader:{
+            version: Number ,
+            previousBlockHeader: String  ,
+            merkleRoot: String,
+            timeStamp: Number ,
+            difficulty: Number,
+            nonce:  Number
+        },
+        txns: Array,
+        currentBlockHash: String
     }
-    // utxo: confirmed transcation
 })
+
+module.exports={transactionSchema}
